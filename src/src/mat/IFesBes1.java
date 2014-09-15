6cdf6125e101f886d71632889c238dd3acd03f34
Evgeniy Shylko <yauheni.shylko@gmail.com>
2014-09-15 19:39:55 +0300

    Commented
    test for Lopatin

--- src/src/mat/IFesBes1.java
+++ src/src/mat/IFesBes1.java
@@ -2,7 +2,7 @@ package mat;
 
 public interface IFesBes1 {
 	int setProfile(mat.Person person);
-	int updateProfile(mat.Person person);
+	int updateProfile(mat.Person person);//Comment test
 	int matLogin(String email, String password);
 	String [] getMattNames(String username);
 	mat.Matt createMatt(mat.MattData data, String username);
