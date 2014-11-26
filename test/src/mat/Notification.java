412e940641531c0a2cba877bf041bef81c88be29
AKAZANSK <aliaksandra.kazanskaya@gmail.com>
2014-11-26 23:26:08 +0200

    updating mat_libs for the test project

---
+++ test/src/mat/Notification.java
@@ -0,0 +1,13 @@
+package mat;
+
+import java.io.Serializable;
+
+public class Notification implements Serializable {
+	public String username;
+	public String tableName;
+	public Notification(){}
+	public Notification (String userName, String tableName){
+		this.username=userName;
+		this.tableName=tableName;
+	}
+}
